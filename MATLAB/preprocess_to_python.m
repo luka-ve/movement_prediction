05f6126c7b5d19cda5bebd0f2d1bdfd4750bbba2
Luka Velinov <luka.velinov@gmail.com>
2021-02-11 16:23:36 +0100

    Changed window splitting method to use moving average

--- MATLAB/preprocess_to_python.m
+++ MATLAB/preprocess_to_python.m
@@ -22,8 +22,10 @@ opts.h5_save_path = fullfile("D:/Coding/Thesis/Data/STFT Output");
 for ppt_file = PPT_FILES
     EEG = pop_loadset('filename', convertStringsToChars(fullfile(DATA_ROOT_PATH, ppt_file)));
     
+    taps_all = concatenate_taps(EEG);
+    
     EEG_clean = clean_EEG(EEG, opts);
-    EEG_data_split = split_EEG(EEG_clean, opts);
+    EEG_data_split = split_EEG(EEG_clean, taps_all, opts);
     EEG_data_split = perform_STFT(EEG_data_split, EEG.srate, opts);
     
     ppt_info = split(ppt_file, "/");
@@ -42,15 +44,21 @@ function EEG_cleaned = clean_EEG(EEG, opts)
     EEG_cleaned = gettechnicallycleanEEG(EEG, opts.highpass_freq, opts.lowpass_freq);
 end
 
+function taps_all = concatenate_taps(EEG)
+    taps_all = [];
+    for i = EEG.Aligned.Phone.Corrected
+        taps_all = vertcat(taps_all, i{1});
+    end
+end
 
-function EEG_data_split = split_EEG(EEG, opts)
+function EEG_data_split = split_EEG(EEG, taps_all, opts)
     % Split file into instances of activity
 
     % Find Taps that are further than max_dist milliseconds apart
     max_dist_between_sections_in_ms = opts.max_dist_between_sections;
     window_padding_size_ms = opts.activity_windows_padding;
     
-    [activity_windows, activity_windows_taps] = find_activity_windows(EEG, max_dist_between_sections_in_ms, window_padding_size_ms);
+    [activity_windows, activity_windows_taps] = find_activity_windows(EEG, taps_all, max_dist_between_sections_in_ms);
 
     % Put split EEG data into multidimensional struct. Each cell of the struct
     % contains one window
@@ -58,19 +66,18 @@ function EEG_data_split = split_EEG(EEG, opts)
     EEG_data_split = struct();
 
     for window = 1:size(activity_windows, 1)
-        EEG_data_split(window).data = EEG.data(:, activity_windows(window, 1):activity_windows(window, 2));
-        EEG_data_split(window).timestamps = activity_windows(window, :);
+        EEG_data_split(window).data = EEG.data(:, activity_windows{window});
+        EEG_data_split(window).timestamps = activity_windows{window};
 
 
         % Add tap timestamps, adjusted to the new timings/indices of the
         % activity windows
         EEG_data_split(window).tap_timestamps = int32(...
-            EEG.Aligned.Phone.Corrected{1, 1}(activity_windows_taps(window, 1):activity_windows_taps(window, 2), 2) - activity_windows(window, 1));
+            taps_all(activity_windows_taps{window}) - activity_windows{window}(1));
     end
 end
 
-
-function [activity_windows, activity_windows_taps] = find_activity_windows(EEG, max_dist, padding_size)
+function [activity_windows, activity_windows_taps] = find_activity_windows(EEG, taps_all, max_dist)
     % FIND_ACTIVITY_WINDOWS Finds windows of tap activity.
     %    Examples:    
     %
@@ -85,34 +92,61 @@ function [activity_windows, activity_windows_taps] = find_activity_windows(EEG, 
     %   Returns:
     %       An n-by-2 matrix containing start and end of activity
     %       windows. Column 1 are start indices, column 2 are end indices.
-    tap_distances = diff(EEG.Aligned.Phone.Corrected{1, 1}(:, 2));
-
-    larger_than_max_dists_idx = find(tap_distances > max_dist);
-    larger_than_max_dists_timestamps = EEG.Aligned.Phone.Corrected{1, 1}(larger_than_max_dists_idx, 2);
-
-
-    % Get indices of activity windows
-    activity_windows = zeros(size(larger_than_max_dists_timestamps, 1) + 1, 2);
-    activity_windows_taps = zeros(size(larger_than_max_dists_timestamps, 1) + 1, 2);
-
-    % Set start of first activity window
-    activity_windows(1, 1) = EEG.Aligned.Phone.Corrected{1, 1}(1, 2) - padding_size;
-    activity_windows_taps(1, 1) = 1;
-
-    % Set end of last activity window
-    activity_windows(end, 2) = EEG.Aligned.Phone.Corrected{1, 1}(end, 2) + padding_size;
-    activity_windows_taps(end, 2) = size(EEG.Aligned.Phone.Corrected{1, 1}, 1);
-
-    for window_start = 2:size(activity_windows, 1)
-
-        activity_windows_taps(window_start, 1) = larger_than_max_dists_idx(window_start - 1) + 1;
-        activity_windows(window_start, 1) = EEG.Aligned.Phone.Corrected{1, 1}(larger_than_max_dists_idx(window_start - 1) + 1, 2) - padding_size;
+    
+    
+    % TODO: Check srate to see if sampling rate was 500 Hz
+    % TODO: Loop over all sessions of tap data
+    
+    
+    % movmean
+    % risetime falltime
+    
+    taps = zeros(size(EEG.data, 2), 1);
+    taps(taps_all(:, 2)) = 1;
+    
+    moving_average_taps = movmean(taps, max_dist * 2);
+    
+    [~, risetimes, ~] = risetime(moving_average_taps, EEG.srate, 'StateLevels', [1e-14, 1e-10]);
+    [~, falltimes, ~] = falltime(moving_average_taps, EEG.srate, 'StateLevels', [1e-14, 1e-10]);
+    
+    activity_windows = {};
+    activity_windows_taps = {};
+    
+    for i = 1:size(risetimes', 2)
+        activity_windows{i} = [ceil(risetimes * EEG.srate), ceil(falltimes * EEG.srate)];
+        activity_windows_taps{i} = find(taps_all(:, 2) > activity_windows{i}(1) & taps_all(:, 2) < activity_windows{i}(2));
     end
-
-    for window_end = 1:(size(larger_than_max_dists_timestamps, 1))
-        activity_windows_taps(window_end, 2) = larger_than_max_dists_idx(window_end);
-        activity_windows(window_end, 2) = larger_than_max_dists_timestamps(window_end) + padding_size;
-    end
+   
+    
+%     tap_distances = diff(EEG.Aligned.Phone.Corrected{1, 1}(:, 2));
+% 
+%     larger_than_max_dists_idx = find(tap_distances > max_dist);
+%     larger_than_max_dists_timestamps = EEG.Aligned.Phone.Corrected{1, 1}(larger_than_max_dists_idx, 2);
+% 
+% 
+%     
+%     
+%     % Get indices of activity windows
+%     activity_windows = zeros(size(larger_than_max_dists_timestamps, 1) + 1, 2);
+%     activity_windows_taps = zeros(size(larger_than_max_dists_timestamps, 1) + 1, 2);
+% 
+%     % Set start of first activity window
+%     activity_windows(1, 1) = EEG.Aligned.Phone.Corrected{1, 1}(1, 2) - padding_size;
+%     activity_windows_taps(1, 1) = 1;
+% 
+%     % Set end of last activity window
+%     activity_windows(end, 2) = EEG.Aligned.Phone.Corrected{1, 1}(end, 2) + padding_size;
+%     activity_windows_taps(end, 2) = size(EEG.Aligned.Phone.Corrected{1, 1}, 1);
+% 
+%     for window_start = 2:size(activity_windows, 1)
+%         activity_windows_taps(window_start, 1) = larger_than_max_dists_idx(window_start - 1) + 1;
+%         activity_windows(window_start, 1) = EEG.Aligned.Phone.Corrected{1, 1}(larger_than_max_dists_idx(window_start - 1) + 1, 2) - padding_size;
+%     end
+% 
+%     for window_end = 1:(size(larger_than_max_dists_timestamps, 1))
+%         activity_windows_taps(window_end, 2) = larger_than_max_dists_idx(window_end);
+%         activity_windows(window_end, 2) = larger_than_max_dists_timestamps(window_end) + padding_size;
+%     end
 end
 
 
@@ -126,7 +160,7 @@ function EEG_data_split = perform_STFT(EEG_data_split, sampling_rate, opts)
         FFTLength = opts.stft_FFTLength;
 
         [EEG_stft, freqs, times] = stft(...
-            EEG_data_split(idx).data', sampling_rate,...
+            EEG_data_split(idx).data, sampling_rate,...
             "Window", window,...
             "OverlapLength", overlap,...
             'FFTLength', FFTLength);
