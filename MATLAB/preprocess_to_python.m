aed4814bac63d28c3bd7182a198a07a91203c516
Luka Velinov <luka.velinov@gmail.com>
2021-02-11 18:05:54 +0100

    Added timing to console logging

--- MATLAB/preprocess_to_python.m
+++ MATLAB/preprocess_to_python.m
@@ -2,9 +2,11 @@
 % github.com/codelableidenvelux/neubee/master
 % EEGLAB
 
+script_start_time = tic();
+
 DATA_ROOT_PATH = "D:/Coding/Thesis/Data/EEG";
-PPT_FILES = ["DS99/08_05_25_04_19.set"];
-%PPT_FILES = ["DS99/08_05_25_04_19.set", "DS99/10_19_25_04_19.set"];
+%PPT_FILES = ["DS99/08_05_25_04_19.set"];
+PPT_FILES = ["DS99/08_05_25_04_19.set", "DS99/10_19_25_04_19.set", "DS95/12_02_11_04_19.set"];
 
 opts = struct();
 
@@ -21,6 +23,8 @@ opts.h5_save_path = fullfile("D:/Coding/Thesis/Data/STFT Output");
 
 
 for ppt_file = PPT_FILES
+    ppt_time_start = tic();
+    
     EEG = pop_loadset('filename', convertStringsToChars(fullfile(DATA_ROOT_PATH, ppt_file)));
     EEG_clean = gettechnicallycleanEEG(EEG, opts.highpass_freq, opts.lowpass_freq);
     
@@ -32,7 +36,7 @@ for ppt_file = PPT_FILES
     
     export_as_h5(EEG_data_split_stft, ppt_info(1), ppt_info(2), opts);
     
-    fprintf("%s exported to %s\n", ppt_file, opts.h5_save_path);
+    fprintf("""%s"" exported to ""%s"" in %.2f seconds.\n", ppt_file, opts.h5_save_path, toc(ppt_time_start));
 end
 
-fprintf("Finished.");
+fprintf("Finished in %.2f seconds.\n", toc(script_start_time));
