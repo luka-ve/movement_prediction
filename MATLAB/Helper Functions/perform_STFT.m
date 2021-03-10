ec2b8f0de6ef79899bd4125d4d95fc54760bf243
Luka Velinov <luka.velinov@gmail.com>
2021-03-10 13:54:59 +0100

    Optimization

--- MATLAB/Helper Functions/perform_STFT.m
+++ MATLAB/Helper Functions/perform_STFT.m
@@ -16,10 +16,18 @@ function EEG_data_split = perform_STFT(EEG_data_split, sampling_rate, opts)
         % Remove negative frequencies
         EEG_stft = EEG_stft(ceil(size(EEG_stft, 1)/2 + 1):end, :, :);
         freqs = freqs(ceil(size(freqs, 1)/2 + 1):end);
+        
+        EEG_stft = log(abs(EEG_stft).^2);
+        
+        % Remove frequency bands above the lowpass filter frequency
+        freqs_to_keep = freqs < opts.lowpass_freq;
+        EEG_stft = EEG_stft(freqs_to_keep, :, :);
 
         % Save stft info into struct
-        EEG_data_split(idx).stft = log(abs(EEG_stft).^2);
+        EEG_data_split(idx).stft = EEG_stft;
         EEG_data_split(idx).freqs = freqs;
         EEG_data_split(idx).times = times;
+        
+        clear EEG_stft freqs times freqs_to_keep;
     end
 end
