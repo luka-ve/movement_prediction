e8ae5fbcddd53ac39461f8a0e76cb31d0e14f9f2
luka-velinov <luka.velinov@gmail.com>
2021-05-18 11:45:47 +0200

    Removed channel interpolation step

--- MATLAB/Helper Functions/gettechnicallycleanEEG.m
+++ MATLAB/Helper Functions/gettechnicallycleanEEG.m
@@ -39,7 +39,7 @@ Rej_Comp = unique([rej]);
 INEEG = pop_subcomp(INEEG,Rej_Comp,0);
 
 % interpolate missing channels 
-INEEG = pop_interp(INEEG,INEEG.Orignalchanlocs,'spherical');
+%INEEG = pop_interp(INEEG,INEEG.Orignalchanlocs,'spherical');
 
 % Final Filter
 [INEEG] = pop_eegfiltnew(INEEG, hp_freq, lp_freq);
