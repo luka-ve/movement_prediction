43f86866c38559951558d76ba47e592f013120cc
luka-velinov <luka.velinov@gmail.com>
2021-06-16 20:38:50 +0200

    Added ability to consider minimum tap intervals

--- MATLAB/Helper Functions/get_FS_taps.m
+++ MATLAB/Helper Functions/get_FS_taps.m
@@ -1,15 +1,19 @@
-function [indices] = get_FS_taps(FS_data, sampling_rate)
+function [indices] = get_FS_taps(FS_data, sampling_rate, min_tap_distance)
 %GET_FS_TAPS Extracts tap events from force sensor data
 %   Extracts tap events from continuous force sensor data.
 % Input:
 %   FS_data: A vector of continuous force sensor data.
 %   sampling_rate: Sampling rate of the force sensor data.
+%   min_tap_distance: Minimum time after a tap before a new tap can be
+%       positively detected.
 %
 % Output:
 %   tap_indices: A vector of indices of extracted tap events.
 %
 % Credits: Original code by Ruchella Kock, adapted by Luka Velinov
 
+
+assert(min_tap_distance > 0);
 
 base = -1; % Base value when force sensor has null force.
 
@@ -34,8 +38,7 @@ FS_data = FS_data(2:(end-1));
 
 %% Remove peaks that are shorter than 5ms. Those are likely noise.
 [pulse_width, initcros, ~] = pulsewidth(FS_data, sampling_rate);
-min_touch_length = 5; % In milliseconds
-indices = round(initcros(pulse_width > (min_touch_length / sampling_rate)) * sampling_rate);
+indices = round(initcros(pulse_width > (min_tap_distance / sampling_rate)) * sampling_rate);
 
 end
 
